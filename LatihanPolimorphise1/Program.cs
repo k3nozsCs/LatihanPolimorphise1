2540e0a578a438c43bdb170fc5f501afb9e6bb5f
k3nozsCs <tarakenzo3@gmail.com>
2026-04-08 14:46:59 +0700

    Polimorphise
    Membuat method polimorphise

--- LatihanPolimorphise1/Program.cs
+++ LatihanPolimorphise1/Program.cs
@@ -1,1 +1,41 @@
-﻿Console.WriteLine("Hello, World!");
+﻿ImageProcessor Kertas = new ImageProcessor();
+
+Kertas.Resize(-50);
+Kertas.Resize(50);
+Kertas.Resize(150);
+Kertas.Resize(100);
+Kertas.Resize(192, 108);
+Kertas.Resize("A4");
+
+class ImageProcessor
+{
+    public void Resize(int presentase)
+    {
+        if (presentase <= 0)
+        {
+            Console.WriteLine("Error! Presentase tidak bolah 0 atau negatif.");
+        }
+        else if (presentase < 100)
+        {
+            Console.WriteLine($"Gambar berhasil diperkecil sebesar {presentase}%");
+        }
+        else if (presentase > 100)
+        {
+            Console.WriteLine($"Gambar berhasil diperbesar sebesar {presentase}%.");
+        }
+        else
+        {
+            Console.WriteLine("Ukuran tetao 100%, tidak ada perubahan");
+        }
+    }
+
+    public void Resize(int lebar, int tinggi)
+    {
+        Console.WriteLine($"Mengubah ukuran menjadi {lebar}mm x {tinggi}mm");
+    }
+
+    public void Resize(string kertas)
+    {
+        Console.WriteLine($"Mengubah ukuran menjadi format kertas {kertas}");
+    }
+}
